5eaff5df48e29338b29d5fdc849f4fcd44261220
joshua2409 <joshuaa.pyy@gmail.com>
2025-04-03 13:52:46 +0700

    test push

--- Frontend/trava/src/home/Home.js
+++ Frontend/trava/src/home/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+const popDes = [
+    {image : '1', title : '1', desc : '1'},
+    {image : '2', title : '2', desc : '2'},
+    {image : '3', title : '3', desc : '3'},
+    {image : '4', title : '4', desc : '4'},
+]
+const recEx = []
+const hidGem = []
 const Home = () => {
   return (
-    <div className='text-red-400 bg-red-700'>Home 123</div>
+    <div>
+        <div>Banner 1</div>
+        <div>Banner 1</div>
+        <div>Explore
+            <div>Popular destinations</div>
+
+        </div>
+    </div>
   )
 }
 
---
+++ Frontend/trava/src/home/homeComponent/RowData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const rowData = () => {
+  return (
+    <div>rowData</div>
+  )
+}
+
+export default rowData
