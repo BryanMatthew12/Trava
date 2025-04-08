6199c94c043f0c1dc20a90191c64fec05e1b748e
BryanMatthew12 <bryanmatthew01@gmail.com>
2025-04-08 14:54:12 +0700

    create services

---
+++ Backend/laravel/app/Services/AdminService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+class AdminService
+{
+    public function __construct()
+    {
+        // Inject models or repositories here if needed
+    }
+
+    // Add your business logic methods here
+}
