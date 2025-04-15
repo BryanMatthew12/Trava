87e910c31a436c37da48ff4ef13489b51cb928ec
joshua2409 <joshuaa.pyy@gmail.com>
2025-04-15 14:19:23 +0700

    add query by place id

--- Backend/laravel/app/Http/Controllers/Api/V1/PlacesController.php
+++ Backend/laravel/app/Http/Controllers/Api/V1/PlacesController.php
@@ -13,20 +13,24 @@ class PlacesController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-{
-    // Check if the 'destination_id' query parameter is present
-    $destinationId = request()->query('destination_id');
-
-    if ($destinationId) {
-        // Filter places by destination_id if the parameter is provided
-        $places = Places::where('destination_id', $destinationId)->with('category')->get();
-    } else {
-        // Return all places if no destination_id is provided
-        $places = Places::with('category')->get();
+    {
+        // Check if the 'destination_id' or 'place_id' query parameter is present
+        $destinationId = request()->query('destination_id');
+        $placeId = request()->query('place_id');
+    
+        if ($placeId) {
+            // Filter places by place_id if the parameter is provided
+            $places = Places::where('place_id', $placeId)->with('category')->get();
+        } elseif ($destinationId) {
+            // Filter places by destination_id if the parameter is provided
+            $places = Places::where('destination_id', $destinationId)->with('category')->get();
+        } else {
+            // Return all places if no parameters are provided
+            $places = Places::with('category')->get();
+        }
+    
+        return response()->json($places);
     }
-
-    return response()->json($places);
-}
 
     /**
      * Show the form for creating a new resource.
